d794f9674d351407adf0b537022e8f778ee0d49c
ayushhh321 <ayush56039@gmail.com>
2025-02-13 20:01:37 +0530

    V3

---
+++ calc.py
@@ -0,0 +1,9 @@
+num1 = input("Enter your number 1 : ") #in this case value input as string hoga then if you add two number it will concatnate
+
+num1 = int(input("Num1: ")) #now num1 will be typecasted to integer
+
+num2 = int(input("Num2: "))
+
+sum = num1+ num2
+
+print("sum of two numnbers is: ", sum)
