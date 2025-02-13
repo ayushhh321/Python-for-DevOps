347eb3e76464ac0cf4e3c52f7c389c176153d571
ayushhh321 <ayush56039@gmail.com>
2025-02-13 20:04:21 +0530

    V4

--- calc.py
+++ calc.py
@@ -7,3 +7,19 @@ num2 = int(input("Num2: "))
 sum = num1+ num2
 
 print("sum of two numnbers is: ", sum)
+
+Difference = num1-num2
+
+print("Difference of two numnbers is: ", Difference)
+
+product = num1* num2
+
+print("product of two numnbers is: ", product)
+
+remenainder = num1% num2
+
+print("remenainder of two numnbers is: ", remenainder)
+
+quotient = num1/num2
+
+print("quotient of two numnbers is: ", quotient)
