2df830b29c13a80f65ded3cee6d5d6f5f3f57768
ayushhh321 <ayush56039@gmail.com>
2025-02-13 21:10:42 +0530

    list

--- loops.py
+++ loops.py
@@ -1,1 +1,11 @@
-list = multiple data set .
+list_of_cloud = ["aws","azure","gcp"]
+print(list_of_cloud)
+
+#adding element to list
+list_of_cloud.append("salesforce")
+print(list_of_cloud)
+
+#adding element at position 
+list_of_cloud.insert(2,"Alibaba")
+print(list_of_cloud)
+
