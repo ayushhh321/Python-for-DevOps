6f45979927bc7d5238b118baaeb4559b24b7398f
ayushhh321 <ayush56039@gmail.com>
2025-02-13 20:13:08 +0530

    update

--- Conditionals.py
+++ Conditionals.py
@@ -1,9 +1,9 @@
 #using upper() and lower()
 
 day_of_week= input("Enter the day of week: ").lower() #anything you enter will be converted into lower case only
-print(day_of_week)
+# print(day_of_week)
 
-if day_of_week=="SATURDAY" or "Sunday":
+if day_of_week=="saturday" or day_of_week=="sunday":
   print("Weekend")
 else:
   print("Not weekend")
